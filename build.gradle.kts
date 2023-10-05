952884d04b833cef2778ca523b2c3b21c17402dc
Akiranya <porridgen@gmail.com>
2023-10-05 13:34:57 +0800

    Update gradle convention plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     val mewcraft = "1.0.0"
+    id("cc.mewcraft.repo-conventions") version mewcraft
     id("cc.mewcraft.java-conventions") version mewcraft
-    id("cc.mewcraft.repository-conventions") version mewcraft
     id("cc.mewcraft.publishing-conventions") version mewcraft
     id("cc.mewcraft.deploy-conventions") version mewcraft
     id("cc.mewcraft.paper-plugins") version mewcraft
